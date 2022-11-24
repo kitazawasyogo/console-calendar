da3b8c79c102eb6e04eb0c18b3866a5bdb4ac5eb
KITAZAWA Syogo <kitazawa@daniel-soft.com>
2022-11-24 17:52:41 +0900

    例外処理の追加

--- DisplayCalendar.vb
+++ DisplayCalendar.vb
@@ -9,23 +9,21 @@ Public Class DisplayCalendar
     Private Const PROMPTING_MASSAGE As String = "年月を[yyyy/mm]で入力してください"
 
     ''' <summary>
+    ''' 警告メッセージ定義
+    ''' </summary>
+    Private Const WARNING_MASSAGE As String = "[yyyy/mm]のフォーマット外です"
+
+    ''' <summary>
     ''' 月のカレンダー表示
     ''' </summary>
     Public Sub ShowCalendar()
 
         Console.WriteLine(PROMPTING_MASSAGE)
-        Dim inputValue As String = Console.ReadLine()
 
-        Dim inputArray As String() = inputValue.Split("/"c)
+        Dim inputValueDisplay As String = ConfirmInputValue()
+        Dim inputArray As String() = inputValueDisplay.Split("/"c)
 
-        If Not inputArray.Length = 2 OrElse Not IsNumeric(inputArray(0)) OrElse Not IsNumeric(inputArray(1)) Then
-
-            MsgBox(PROMPTING_MASSAGE)
-            Return
-
-        End If
-
-        Dim beginningDay As String = (inputValue & "/01")
+        Dim beginningDay As String = (inputValueDisplay & "/01")
         Dim beginningDt As DateTime = DateTime.Parse(beginningDay)
         Dim beginningWeek As DayOfWeek = beginningDt.DayOfWeek
 
@@ -35,7 +33,7 @@ Public Class DisplayCalendar
 
         For flowOfDays As Integer = 1 To daysInMonth
 
-            Dim day As String = (inputValue & flowOfDays.ToString("/00"))
+            Dim day As String = (inputValueDisplay & flowOfDays.ToString("/00"))
             Dim dt As DateTime = DateTime.Parse(day)
 
             Console.Write(String.Format("{0, 4}", flowOfDays))
@@ -46,4 +44,34 @@ Public Class DisplayCalendar
         Next
 
     End Sub
+
+    ''' <summary>
+    ''' 例外処理と入力値の受け渡し
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function ConfirmInputValue() As String
+
+        Dim inputValue As String = Nothing
+
+        While True
+
+            Try
+
+                inputValue = Console.ReadLine()
+
+                Dim inputArray As String() = inputValue.Split("/"c)
+
+                If Not inputArray.Length = 2 OrElse Not IsNumeric(inputArray(0)) OrElse Not IsNumeric(inputArray(1)) OrElse Not inputArray(0).Length = 4 OrElse Not inputArray(1).Length = 2 Then
+                    Throw New ArgumentException
+                Else
+                    Return inputValue
+                End If
+
+            Catch ex As ArgumentException
+                Console.WriteLine(WARNING_MASSAGE)
+            End Try
+
+        End While
+
+    End Function
 End Class
